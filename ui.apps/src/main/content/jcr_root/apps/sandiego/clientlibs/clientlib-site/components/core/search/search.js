f361976e777e31510f720a61d8bdbdd0267cc2db
Loc Tran <loc.tran@technocom.asia>
2019-07-15 17:59:41 +0700

    Update code landing page

--- ui.apps/src/main/content/jcr_root/apps/sandiego/clientlibs/clientlib-site/components/core/search/search.js
+++ ui.apps/src/main/content/jcr_root/apps/sandiego/clientlibs/clientlib-site/components/core/search/search.js
@@ -1,7 +1,7 @@
 (function (element, $) {
   'use strict';
   var target = $(element),
-    placeholder = "Search for document that you are looking for....";
+    placeholder = "Search for document you are looking for....";
   target.attr('placeholder', placeholder);
 
 }('.cmp-search--landing-hero input', jQuery));
