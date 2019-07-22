83a0b2ce1ae5b75afc85f834fef97dd68df024a6
Loc Tran <loc.tran@technocom.asia>
2019-07-22 17:11:55 +0700

    Change link of items

--- ui.apps/src/main/content/jcr_root/apps/sandiego/clientlibs/clientlib-site/components/secure-page/home/calendar-item/calendar-item.js
+++ ui.apps/src/main/content/jcr_root/apps/sandiego/clientlibs/clientlib-site/components/secure-page/home/calendar-item/calendar-item.js
@@ -3,84 +3,84 @@
     day: '17',
     month: 'Jul,2019',
     title: 'This is the subject of a meeting for approving documents of Core EA',
-    fileLink: 'https://www.youtube.com/watch?v=knW7-x7Y7RE',
+    fileLink: 'https://www.fshare.vn/file/ICXCLPJBP9YP?token=1563783680',
     fileName: 'Agenda.pdf'
 }, {
     id: 2,
     day: '15',
     month: 'Jul,2019',
     title: 'This is the subject of a meeting for approving documents of Core EA',
-    fileLink: 'https://www.youtube.com/watch?v=knW7-x7Y7RE',
+    fileLink: 'https://www.fshare.vn/file/ICXCLPJBP9YP?token=1563783680',
     fileName: 'Haytraochoanh.pdf'
 }, {
     id: 3,
     day: '14',
     month: 'Jul,2019',
     title: 'This is the subject of a meeting for approving documents of Core EA',
-    fileLink: 'https://www.youtube.com/watch?v=knW7-x7Y7RE',
+    fileLink: 'https://www.fshare.vn/file/ICXCLPJBP9YP?token=1563783680',
     fileName: 'aem.pdf'
 }, {
     id: 4,
     day: '13',
     month: 'Jul,2019',
     title: 'This is the subject of a meeting for approving documents of Core EA',
-    fileLink: 'https://www.youtube.com/watch?v=knW7-x7Y7RE',
+    fileLink: 'https://www.fshare.vn/file/ICXCLPJBP9YP?token=1563783680',
     fileName: 'Agenda.pdf'
 }, {
     id: 1,
     day: '12',
     month: 'Jul,2019',
     title: 'This is the subject of a meeting for approving documents of Core EA',
-    fileLink: 'https://www.youtube.com/watch?v=knW7-x7Y7RE',
+    fileLink: 'https://www.fshare.vn/file/ICXCLPJBP9YP?token=1563783680',
     fileName: 'Agenda.pdf'
 }, {
     id: 2,
     day: '11',
     month: 'Jul,2019',
     title: 'This is the subject of a meeting for approving documents of Core EA',
-    fileLink: 'https://www.youtube.com/watch?v=knW7-x7Y7RE',
+    fileLink: 'https://www.fshare.vn/file/ICXCLPJBP9YP?token=1563783680',
     fileName: 'Haytraochoanh.pdf'
 }, {
     id: 3,
     day: '10',
     month: 'Jul,2019',
     title: 'This is the subject of a meeting for approving documents of Core EA',
-    fileLink: 'https://www.youtube.com/watch?v=knW7-x7Y7RE',
+    fileLink: 'https://www.fshare.vn/file/ICXCLPJBP9YP?token=1563783680',
     fileName: 'aem.pdf'
 }, {
     id: 4,
     day: '19',
     month: 'Jul,2019',
     title: 'This is the subject of a meeting for approving documents of Core EA',
-    fileLink: 'https://www.youtube.com/watch?v=knW7-x7Y7RE',
+    fileLink: 'https://www.fshare.vn/file/ICXCLPJBP9YP?token=1563783680',
     fileName: 'Agenda.pdf'
 }, {
     id: 1,
     day: '12',
     month: 'Jul,2019',
     title: 'This is the subject of a meeting for approving documents of Core EA',
-    fileLink: 'https://www.youtube.com/watch?v=knW7-x7Y7RE',
+    fileLink: 'https://www.fshare.vn/file/ICXCLPJBP9YP?token=1563783680',
     fileName: 'Agenda.pdf'
 }, {
     id: 2,
     day: '11',
     month: 'Jul,2019',
     title: 'This is the subject of a meeting for approving documents of Core EA',
-    fileLink: 'https://www.youtube.com/watch?v=knW7-x7Y7RE',
+    fileLink: 'https://www.fshare.vn/file/ICXCLPJBP9YP?token=1563783680',
     fileName: 'Haytraochoanh.pdf'
 }, {
     id: 3,
     day: '10',
     month: 'Jul,2019',
     title: 'This is the subject of a meeting for approving documents of Core EA',
-    fileLink: 'https://www.youtube.com/watch?v=knW7-x7Y7RE',
+    fileLink: 'https://www.fshare.vn/file/ICXCLPJBP9YP?token=1563783680',
     fileName: 'aem.pdf'
 }, {
     id: 4,
     day: '19',
     month: 'Jul,2019',
     title: 'This is the subject of a meeting for approving documents of Core EA',
-    fileLink: 'https://www.youtube.com/watch?v=knW7-x7Y7RE',
+    fileLink: 'https://www.fshare.vn/file/ICXCLPJBP9YP?token=1563783680',
     fileName: 'Agenda.pdf'
 }];
 
@@ -99,26 +99,18 @@
     $.each(list_items, function (index, item) {
 
       $("<div class='cmp-calendar-item'>" +
-        "<div class='cmp-calendar-item__container'>" +
-        "<div class='cmp-calendar-item__date'>" +
-        "<div>" +
-        item.day +
-        "</div>" +
-        "<div>" +
-        item.month +
-        "</div>" +
-        "</div>" +
-
-        "<div>" +
-        "<div class='cmp-calendar-item__title'>" +
-        item.title +
-        "</div>" +
-
-        "<div>" +
-        "<a href='" + item.fileLink + "' class='cmp-calendar-item__file'>" + item.fileName + "</a>" +
-        "</div>" +
-        "</div>" +
-        "</div>" +
+        "  <div class='cmp-calendar-item__container'>" +
+        "    <div class='cmp-calendar-item__date'>" +
+        "      <div>" + item.day + "</div>" +
+        "      <div>" + item.month + "</div>" +
+        "    </div>" +
+        "    <div>" +
+        "      <div class='cmp-calendar-item__title'>" + item.title + "</div>" +
+        "      <div class='cmp-calendar-item__file'>" +
+        "      <a href='" + item.fileLink + "' target='_blank'>" + item.fileName + "</a>" +
+        "      </div>" +
+        "    </div>" +
+        "  </div>" +
         "</div>").appendTo(target);
 
       //  break after 3 elements
