7e8e41168d5398ce1cd4da4516a2a5ae5227600c
Than Dao <than.dao@technocom.asia>
2019-07-22 15:49:44 +0700

    Update notification

--- ui.apps/src/main/content/jcr_root/apps/sandiego/clientlibs/clientlib-site/components/secure-page/home/notification-item/notification-item.js
+++ ui.apps/src/main/content/jcr_root/apps/sandiego/clientlibs/clientlib-site/components/secure-page/home/notification-item/notification-item.js
@@ -1,64 +1,69 @@
-  var list_items = [{
+var list_items = [
+  {
     id: 1,
     type: 'NOD',
     title: 'This is the subject of a notification for approving documents.',
     time: '5 mins ago',
     status: 'Submitted'
-}, {
+  },
+  {
     id: 2,
     type: 'SDD',
     title: 'This is the subject of a notification for approving documents.',
     time: '15 mins ago',
     status: 'Internal accepted'
-}, {
+  },
+  {
     id: 3,
     type: 'SDD',
     title: 'This is the subject of a notification for approving documents.',
     time: '25 mins ago',
     status: 'Rejected'
-}, {
+  },
+  {
     id: 4,
     type: 'NOD',
     title: 'This is the subject of a notification for approving documents.',
     time: '30 mins ago',
     status: 'Submitted'
-}];
+  }
+];
 
+(function notification_item(element, list_items, $) {
+  'use strict';
+  var target = $(element);
 
+  //    if (editmode.isEditMode) {
+  //      return;
+  //    } else if (editmode.isPreviewMode) {
+  //      return;
+  //    }
 
-  (function notification_item(element, list_items, $) {
-    'use strict';
-    var target = $(element);
-
-    //    if (editmode.isEditMode) {
-    //      return;
-    //    } else if (editmode.isPreviewMode) {
-    //      return;
-    //    }
-
-
-    $.each(list_items, function (index, item) {
-
-      $("<div class='cmp-notification-item'>" +
+  $.each(list_items, function(index, item) {
+    $(
+      "<div class='cmp-notification-item'>" +
         "<div class='cmp-notification-item__container'>" +
         "<div class='cmp-notification-item__type'>" +
         item.type +
-        "</div>" +
-        "<div>" +
+        '</div>' +
+        '<div>' +
         "<div class='cmp-notification-item__title'>" +
         item.title +
-        "</div>" +
-        "<div>" +
-        "<div class='cmp-notification-item__time'>" + item.time + "</div>" +
-        "<div class='cmp-notification-item__status'>" + item.status + "</div>" +
-        "</div>" +
-        "</div>" +
-        "</div>" +
-        "</div>").appendTo(target);
+        '</div>' +
+        '<div>' +
+        "<div class='cmp-notification-item__time'>" +
+        item.time +
+        '</div>' +
+        "<div class='cmp-notification-item__status'>" +
+        item.status +
+        '</div>' +
+        '</div>' +
+        '</div>' +
+        '</div>' +
+        '</div>'
+    ).appendTo(target);
 
-      //  break after 3 elements
-      return (index !== 1)
-    })
-
-
-  }('.notification-item', list_items, jQuery));
+    //  break after 3 elements
+    return index !== 1;
+  });
+})('.notification-item', list_items, jQuery);
