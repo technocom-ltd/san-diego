0253814996bdc5e3e728b295654802f3a18fdda7
Loc Tran <loc.tran@technocom.asia>
2019-07-31 10:31:41 +0700

    update it services catalog page v2

--- ui.apps/src/main/content/jcr_root/apps/sandiego/components/content/public-page/it-services-page-v1/sample/sample.js
+++
@@ -1,9 +0,0 @@
-
-"use strict";
-use(function () {
-    var info = {};    
-    info.title = properties["jcr:title"];
-    info.fileName = properties["fileReference"];
-    info.description = resource.properties["description"];    
-    return info;
-})
